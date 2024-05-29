6a548d7a451251d9136db10a6b7db6a5e2c6dabe
Tábata Picado Carmona <tabata.picado@ucr.ac.cr>
2024-05-29 10:53:14 -0600

    Feat: Se grafica la curva ROC.

--- Infografía/infrografia.R
+++ Infografía/infrografia.R
@@ -5,14 +5,22 @@ install.packages("caret")
 library(caret)
 library(pROC)
 library(ggplot2)
+library(ROCR)
+library(precrec)
 
 datos <- read_excel("Salud mental estudiantes.xlsx")
 datos <- datos[, -c(1,6)]
-datos <- as.data.frame(lapply(datos, as.factor))
 
 colnames(datos) <- c("Género", "Edad", "Carrera", "Año de carrera", 
                      "Estado civil", "Depresión", "Ansiedad", 
                      "Ataques de pánico", "Buscar especialista")
+
+# Rellenar los valores NA en la columna "Edad" con la media
+media_edad <- mean(datos$Edad, na.rm = TRUE)
+datos$Edad[is.na(datos$Edad)] <- round(media_edad)
+
+# Convertir a factores las columnas 
+datos <- as.data.frame(lapply(datos, as.factor))
 
 modelo.logit <- glm(Depresión ~ ., data = datos, family = "binomial")
 summary(modelo.logit)
@@ -33,7 +41,7 @@ summary(modelo.logit)
 # con base al resultado de la regresión.
 
 # Se puede interpretar que una persona que tiene 23 años de edad, cuyo parámetro es
-#  40.5080 , tiene e^40.5080 ≈ 3.912018e+17 veces más de probabilidad de tener depresión que de no
+#  40.4898 , tiene e^40.4898 ≈ 3.841464e+17 veces más de probabilidad de tener depresión que de no
 # con base al resultado de la regresión.
 
 # Se puede interpretar que un universitario hombre, cuyo parámetro es
@@ -45,9 +53,14 @@ summary(modelo.logit)
 
 # Predecir las clases
 predicciones <- predict(modelo.logit, datos, type = "response")
-
+  
 # Convertir las probabilidades predichas en clases binarias
-predicciones_clases <- ifelse(predicciones > 0.5, "Yes", "No")
+
+obj_roc <- roc(datos$Depresión, predicciones, auc=T, ci=T)
+best_thresh <- coords(obj_roc, "best", 
+                      ret = c("threshold", "sensitivity", "specificity"), 
+                      best.method="youden")
+predicciones_clases <- ifelse(predicciones > 0.3983383, "Yes", "No")
 
 # Crear la matriz de confusión
 confusion_matrix <- confusionMatrix(as.factor(predicciones_clases), datos$Depresión)
@@ -55,16 +68,20 @@ confusion_matrix <- confusionMatrix(as.factor(predicciones_clases), datos$Depres
 # Ver la matriz de confusión
 print(confusion_matrix)
 
-
 # Curva Roc 
 
+precrec_obj <- evalmod(scores = predicciones, labels = datos$Depresión)
+p <- autoplot(precrec_obj)
 
-#Crear el gráfico de la identidad
-plot(c(0, 1), c(0, 1), type = "l", xlab = "1-Especificidad", ylab = "Recall", main = "Curva Roc")
-abline(a = 0, b = 1, col = "blue")  # Línea de identidad (y = x)
+# Añadir el punto óptimo a la curva ROC
+fpr_best <- 1 - best_thresh$specificity
+tpr_best <- best_thresh$sensitivity
 
-# Agregar el punto aleatorio al gráfico
-points(1-0.8857,0.9692, col = "red", pch = 19)
-
+# Añadir el punto al gráfico
+p[[1]] + geom_point(aes(x = fpr_best, y = tpr_best), color = "red", size = 1) +
+  ggtitle("Curva ROC") +
+  xlab("1 - Especificidad") +
+  ylab("Sensibilidad")
+  
 # Dado que el punto Roc está por encima de la recta, significa que es un buen 
 # modelo.

8225b1734ba4fae31903053e64296a68fb90fea8
Tábata Picado Carmona <tabata.picado@ucr.ac.cr>
2024-05-29 18:35:37 -0600

    Fix: Se arregla el umbral para la matriz de confusión y la curva ROC.

--- Infografía/infrografia.R
+++ Infografía/infrografia.R
@@ -25,30 +25,6 @@ datos <- as.data.frame(lapply(datos, as.factor))
 modelo.logit <- glm(Depresión ~ ., data = datos, family = "binomial")
 summary(modelo.logit)
 
-
-# Análisis odd ratios
-
-# Se puede interpretar que una persona con ansiedad, cuyo parámetro es
-#   0.3180 , tiene e^   0.3180  ≈ 1.374376 veces más de probabilidad de tener depresión que de no
-# con base al resultado de la regresión.
-
-# Se puede interpretar que una persona que estudia matemática, cuyo parámetro es
-# -40.1405, tiene e^ -40.1405 ≈ 3.691496e-18 veces más de probabilidad de tener depresión que de no
-# con base al resultado de la regresión.
-
-# Se puede interpretar que una persona que tiene 2 años de estudio en la universidad, cuyo parámetro es
-#   1.3035, tiene e^  1.3035 ≈ 3.682162 veces más de probabilidad de tener depresión que de no
-# con base al resultado de la regresión.
-
-# Se puede interpretar que una persona que tiene 23 años de edad, cuyo parámetro es
-#  40.4898 , tiene e^40.4898 ≈ 3.841464e+17 veces más de probabilidad de tener depresión que de no
-# con base al resultado de la regresión.
-
-# Se puede interpretar que un universitario hombre, cuyo parámetro es
-#   -0.9400 , tiene e^ -0.9400 ≈ 0.3906278 veces más de probabilidad de tener depresión que de no
-# con base al resultado de la regresión
-
-
 # Matriz de confusión
 
 # Predecir las clases
@@ -60,7 +36,7 @@ obj_roc <- roc(datos$Depresión, predicciones, auc=T, ci=T)
 best_thresh <- coords(obj_roc, "best", 
                       ret = c("threshold", "sensitivity", "specificity"), 
                       best.method="youden")
-predicciones_clases <- ifelse(predicciones > 0.3983383, "Yes", "No")
+predicciones_clases <- ifelse(predicciones > 0.2943312, "Yes", "No")
 
 # Crear la matriz de confusión
 confusion_matrix <- confusionMatrix(as.factor(predicciones_clases), datos$Depresión)

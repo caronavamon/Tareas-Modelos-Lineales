e5c32e5c7d984617dde358d9c5d8166235aa06fd
Tábata Picado Carmona <tabata.picado@ucr.ac.cr>
2024-05-31 11:27:49 -0600

    Feat: Se coloca el código ordenado y se le arregla el nombre a las variables.

--- Infografía/infrografia.R
+++ Infografía/infrografia.R
@@ -1,13 +1,13 @@
 library(readr)
 library(readxl)
-install.packages("ipred")
-install.packages("caret")
 library(caret)
 library(pROC)
 library(ggplot2)
 library(ROCR)
 library(precrec)
 library(ggrepel)
+
+#-----------------------------------Datos---------------------------------------
 
 datos <- read_excel("Salud mental estudiantes.xlsx")
 datos <- datos[, -c(1,6)]
@@ -23,40 +23,44 @@ datos$Edad[is.na(datos$Edad)] <- round(media_edad)
 # Convertir a factores las columnas 
 datos <- as.data.frame(lapply(datos, as.factor))
 
+#--------------------------------Modelo-----------------------------------------
+
+# Se realiza el modelo logístico
 modelo.logit <- glm(Depresión ~ ., data = datos, family = "binomial")
 summary(modelo.logit)
 
-# Matriz de confusión
+#-----------------------------Matriz de confusión-------------------------------
 
 # Predecir las clases
 predicciones <- predict(modelo.logit, datos, type = "response")
   
-# Convertir las probabilidades predichas en clases binarias
 
+# Encontrar el umbral adecuado de acuerdo al índice de Youden
 obj_roc <- roc(datos$Depresión, predicciones, auc=T, ci=T)
-best_thresh <- coords(obj_roc, "best", 
+umbral <- coords(obj_roc, "best", 
                       ret = c("threshold", "sensitivity", "specificity"), 
                       best.method="youden")
+
+# Convertir las probabilidades predichas en clases binarias de acuerdo al índice 
+# encontrado. 
 predicciones_clases <- ifelse(predicciones > 0.2943312, "Yes", "No")
 
 # Crear la matriz de confusión
-confusion_matrix <- confusionMatrix(as.factor(predicciones_clases), datos$Depresión)
+matriz_confusion <- confusionMatrix(as.factor(predicciones_clases), datos$Depresión)
+print(matriz_confusion)
 
-# Ver la matriz de confusión
-print(confusion_matrix)
+#-----------------------------Curva Roc----------------------------------------- 
 
-# Curva Roc 
-
-precrec_obj <- evalmod(scores = predicciones, labels = datos$Depresión)
-p <- autoplot(precrec_obj)
+curva_roc <- evalmod(scores = predicciones, labels = datos$Depresión)
+graf_curva_roc <- autoplot(curva_roc)
 
 # Añadir el punto óptimo a la curva ROC
-fpr_best <- 1 - best_thresh$specificity
-tpr_best <- best_thresh$sensitivity
+TFP <- 1 - umbral$specificity
+recall <- umbral$sensitivity
 
-# Añadir el punto al gráfico
-p[[1]] + geom_point(aes(x = fpr_best, y = tpr_best), color = "red", size = 1) +
-  geom_text(aes(x = fpr_best, y = tpr_best, label = sprintf("(%.2f, %.2f)", fpr_best, tpr_best)), 
+# Gráfico curva ROC
+graf_curva_roc[[1]] + geom_point(aes(x = TFP, y = recall), color = "red", size = 1) +
+  geom_text(aes(x = TFP, y = recall, label = sprintf("(%.2f, %.2f)", TFP, recall)), 
                   size = 3, nudge_y = -0.05, nudge_x = 0.20) +
   ggtitle("Curva ROC") +
   xlab("1 - Especificidad") +
